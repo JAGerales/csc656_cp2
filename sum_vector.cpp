b6efe01fec08805baa1de3603547fb417ca04b9f
JAGerales <124013016+JAGerales@users.noreply.github.com>
2023-10-23 18:02:03 -0700

    Update sum_vector.cpp

--- sum_vector.cpp
+++ sum_vector.cpp
@@ -11,14 +11,20 @@
 void 
 setup(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_vector problem_setup, N=%lld \n", N);
+   for (int i = 0; i < N; i++){
+      A[i] = i;
+   }
+   printf(" inside sum_vector problem_setup, N=%ld \n", N);
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_vector perform_sum, N=%lld \n", N);
+   int64_t sum = 0;
+   for (int i = 0; i < N; i++){
+      sum += A[i];
+   }
+   printf(" inside sum_vector perform_sum, N=%ld \n", N);
 
-   return 0;
+   return sum;
 }
-
