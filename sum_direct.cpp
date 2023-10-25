ab52bd9660496c680565faa133dd52b4341e3b69
JAGerales <124013016+JAGerales@users.noreply.github.com>
2023-10-25 12:20:35 -0700

    Update sum_direct.cpp

--- sum_direct.cpp
+++ sum_direct.cpp
@@ -19,6 +19,7 @@ sum(int64_t N, uint64_t A[])
 {
    int64_t sum = 0;
    for (int i = 0; i < N; i++){
+      A[i] = i;
       sum += A[i];
    }
    printf(" inside direct_sum perform_sum, N=%ld \n", N);
